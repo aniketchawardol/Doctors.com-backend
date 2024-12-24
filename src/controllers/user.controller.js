fcb7be1e76dae6c64344aead1e7a492df98c0da0
aniketchawardol <aniketchawardol@gmail.com>
2024-12-24 23:57:41 +0530

    Implement user authentication with login, logout, and token refresh functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,23 @@ import { ApiError } from "../utils/ApiError.js";
 import { Patient } from "../models/patient.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import fs from 'fs';
+import jwt from "jsonwebtoken";
+
+const generateAccessandRefreshToken = async (userId) => {
+  try {
+    const user = await Patient.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Error generating token");
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, password } = req.body;
   if ([fullname, email, password].some((field) => field.trim() === "")) {
@@ -20,22 +36,138 @@ const registerUser = asyncHandler(async (req, res) => {
   let uploadedPhoto;
   if (profilephotolocalpath) {
     uploadedPhoto = await uploadOnCloudinary(profilephotolocalpath);
-    fs.unlinkSync(profilephotolocalpath);
   }
- const patient = await Patient.create({
+  const patient = await Patient.create({
     fullname,
     email,
     password,
     profilephoto: uploadedPhoto?.url || "",
   });
 
-  const createdPatient = await Patient.findById(patient._id).select("-password -refreshToken");
+  const createdPatient = await Patient.findById(patient._id).select(
+    "-password -refreshToken"
+  );
 
   if (!createdPatient) {
     throw new ApiError(500, "Error creating user");
   }
 
-  return res.status(201).json(new ApiResponse(201, createdPatient, "User created successfully"));
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdPatient, "User created successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if ([email, password].some((field) => field.trim() === "")) {
+    throw new ApiError(400, "email and password are required");
+  }
+
+  const user = await Patient.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isMatch = await user.verifyPassword(password);
+
+  if (!isMatch) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await Patient.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await Patient.findByIdAndUpdate(
+    req.user._id,
+    { $set: { refreshToken: undefined } },
+    { new: true }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+  
+    const user = await Patient.findById(decodedToken?._id);
+  
+    if (!user) {
+      throw new ApiError(401, "Invalid user");
+    }
+  
+    if (user.refreshToken !== incomingRefreshToken) {
+      throw new ApiError(401, "expired refresh token");
+    }
+  
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+  
+    const { accessToken, newrefreshToken } = await generateAccessandRefreshToken(
+      user._id
+    );
+  
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newrefreshToken, options)
+      .json(new ApiResponse(200, { accessToken, newrefreshToken },
+        "Access token refreshed successfully"
+      ));
+  } catch (error) {
+    throw new ApiError(401, error.message);
+    
+  }
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,13 +2,19 @@ const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next)
   } catch (error) {
-    res.status(error.code || 500).json({
-      sucess: false,
+    
+    let statusCode = 500;
+    
+    if (error.name === 'ValidationError') statusCode = 400;
+    if (error.name === 'CastError') statusCode = 400;
+    if (error.code === 'ENOENT') statusCode = 404;
+
+
+    res.status(statusCode).json({
+      success: false,
       message: error.message,
     })
   }
 }
 
-
-
-export {asyncHandler}
+export { asyncHandler }
